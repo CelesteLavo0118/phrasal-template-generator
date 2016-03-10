5af41883ca1af37108e6742afc4c547655ae50ec
Celeste Lavoie <celeste.lavoie@gmail.com>
2016-03-10 17:08:46 -0500

    adding html and prompts

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var makeStory = alert('Let us make a story together!');
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+
+var creature = prompt('A creature (dragon, moth, etc.)')
+
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like? a) Snacking b) Escaping c) Winning \n Enter the letter below:');
