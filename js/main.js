6dc2acae9faad982dd5f89feec4bc1657bafcc91
Celeste Lavoie <celeste.lavoie@gmail.com>
2016-03-10 17:09:29 -0500

    adding missing semicolon

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var makeStory = alert('Let us make a story together!');
 
 var colour = prompt('A colour (orange, indigo, etc.)');
 
-var creature = prompt('A creature (dragon, moth, etc.)')
+var creature = prompt('A creature (dragon, moth, etc.)');
 
 var adjective = prompt('An adjective (beautiful, super, etc.)');
 
